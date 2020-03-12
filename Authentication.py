146fa2d3ff8179673ef066ca468584d2791fa963
SuzanAlbedween <suzanalwkily@gmail.com>
2020-03-12 09:21:56 +0200

    create restapi for  Authentication

---
+++ Authentication.py
@@ -0,0 +1,53 @@
+import json
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS,cross_origin
+import random
+from DBuser import *
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+class userDTO:
+
+    def __init__(self,ID,username,userpassword):
+        self.ID=ID
+        self.username=str(username)
+        self.userpassword=str(userpassword)
+#class authentication:
+#****************************
+data=[{"name":"suzi2","password":"12345"},{"name":"bibi","password":"92345"}]
+class authentication(Resource):
+    def get(self,name=None):
+        if(name==None):
+            return data, 200
+        for res in data:
+            if(res["name"] ==name):
+                return data, 200
+        return "name not found", 404
+
+
+
+
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name")
+        parser.add_argument("password")
+        params = parser.parse_args()
+        obj=userDTO(None,params["name"],params["password"])
+        print(params["name"],params["password"])
+        print("the obj is",obj.username,"  ",obj.userpassword)
+        res=CheckLogIn(obj.username,obj.userpassword)
+        #rsjson={"result":json.dumps(res)}
+        #resinjson = json.dumps(res)
+        return str(res), 200
+
+
+
+
+
+api.add_resource(authentication, "/auth", "/auth/", "/auth/<string:name>")
+if __name__ == '__main__':
+    app.run(debug=True)
