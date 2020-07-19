9bc21d882cd1964e9a88db603bdb2085363e60e3
SuzanAlbedween <suzanalwkily@gmail.com>
2020-07-19 19:45:18 +0300

    updating Authentication

--- Authentication.py
+++ Authentication.py
@@ -1,12 +1,14 @@
-import json
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS,cross_origin
-import random
+from flask.views import MethodView
+
 from DBuser import *
+
 app = Flask(__name__)
 CORS(app)
-api = Api(app)
+api = Api(app )
+
 
 
 class userDTO:
@@ -15,9 +17,8 @@ class userDTO:
         self.ID=ID
         self.username=str(username)
         self.userpassword=str(userpassword)
-#class authentication:
-#****************************
-#data=[{"name":"suzi2","password":"12345"},{"name":"bibi","password":"92345"}]
+
+
 class authentication(Resource):
     def post(self):
         parser = reqparse.RequestParser()
@@ -32,11 +33,10 @@ class authentication(Resource):
             print(params["name"], params["password"])
             print("the obj is", obj.username, "  ", obj.userpassword)
             res = CheckLogIn(obj.username, obj.userpassword)
-            # rsjson={"result":json.dumps(res)}
-            # resinjson = json.dumps(res)
             return str(res), 200
 
 
 api.add_resource(authentication, "/auth", "/auth/", "/auth/<string:name>")
+
 if __name__ == '__main__':
     app.run(debug=True)
